9f6fe90ded67215d0262d8e9d399eb2239451675
joesong7 <72059440+joesong77@users.noreply.github.com>
2022-10-28 15:45:22 +0800

    初版

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <div class="appbar">
+
+    </div>
+    <div class="main">
+      <div class="list">
+      
+      </div> 
+      <div class="content">
+        <router-view/>
+          
+        </div>
+    </div>
+
   </div>
-  <router-view/>
+
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<style scoped>
+.main {
+  display: flex;
+  flex-direction: row;
+  height: 100vh;
 }
 
-#nav {
-  padding: 30px;
+.list{
+
+  flex:1;
+  display:flex;
+  flex-direction: column;
+  background-color: #2c3e50;
+  
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.content{
+  
+  flex: 6;
+  display: flex;
+  flex-direction: column;
+
+  background-color:firebrick;
+
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
